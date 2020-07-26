38691d83a25dbd465b12bd99d832cae67fe50cab
jjones-ts <jjones@thinkstack.co>
2020-07-26 13:44:21 -0400

    added type back in

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "access_key" {
+  type        = string
   description = "Access key for AWS."
 }
 
 variable "secret_key" {
+  type        = string
   description = "Secret key for AWS. "
 }
