9eb18b23700a6074618734736c643b85e9feca6c
jjones-ts <jjones@thinkstack.co>
2020-07-26 13:31:40 -0400

    remooved type

--- variables.tf
+++ variables.tf
@@ -1,9 +1,7 @@
 variable "access_key" {
-  type        = string
   description = "Access key for AWS."
 }
 
 variable "secret_key" {
-  type        = string
-  description = "Secret key for AWS."
+  description = "Secret key for AWS. "
 }
