cf88c8bfaa8191223e3aaa96939073db9a5e1a68
jjones-ts <jjones@thinkstack.co>
2020-08-21 15:10:14 -0400

    testing out session manager code.

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "jakeasarus"
+
+    workspaces {
+      name = "test"
+    }
+  }
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-123123142152352123123152"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
